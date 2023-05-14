faf229fa730038b10db400e1f417bd56b8c6a384
Melt <seokwoo2000@naver.com>
2023-05-14 20:27:49 +0900

    [Add] 외부 링크 Constant 구조체 추가 #134

---
+++ ZOOC/ZOOC/Global/Constant/EnternalURL.swift
@@ -0,0 +1,20 @@
+//
+//  EnternalURL.swift
+//  ZOOC
+//
+//  Created by 장석우 on 2023/05/14.
+//
+
+import Foundation
+
+
+//TODO: 기획이 주는 URL으로 수정해야함.
+
+struct ExternalURL {
+    static let zoocDefaultURL = "https://www.notion.so/melt-split/ZOOC-536c179bf9f54b328f1fe6eabdd58ac7?pvs=4"
+    static let termsOfUse = "https://github.com/meltsplit?tab=stars"
+    static let privacyPolicy = "https://melt-split.notion.site/535bbdf839d14ae791014ffccb01f146"
+    
+    static let meltGithub = "https://github.com/meltsplit"
+    static let hidiGithub = "https://github.com/HELLOHIDI"
+}

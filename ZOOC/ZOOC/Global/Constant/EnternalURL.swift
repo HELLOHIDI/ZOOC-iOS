404329d59841129d823a043b1b02b6f6cff85291
Melt <seokwoo2000@naver.com>
2023-05-24 12:48:47 +0900

    [Chore]  외부링크 수정 #149

--- ZOOC/ZOOC/Global/Constant/EnternalURL.swift
+++ ZOOC/ZOOC/Global/Constant/EnternalURL.swift
@@ -11,10 +11,10 @@ import Foundation
 //TODO: 기획이 주는 URL으로 수정해야함.
 
 struct ExternalURL {
-    static let zoocDefaultURL = "https://www.notion.so/melt-split/ZOOC-536c179bf9f54b328f1fe6eabdd58ac7?pvs=4"
-    static let termsOfUse = "https://www.notion.so/5513105c69e2439788fca63b446b3c97?pvs=4"
-    static let privacyPolicy = "https://www.notion.so/813f008808174b63b5c96867bf8ff053?pvs=4"
-    static let consentMarketing = "https://www.notion.so/97b6caa8eec14ebf93b63c5d6ddee909?pvs=4"
+    static let zoocDefaultURL = "https://www.notion.so/ZOOC-697b946f29f14929bd77325b181d90b0?pvs=4"
+    static let termsOfUse = "https://www.notion.so/5076c3f7309743bebb5995a189cdb2be?pvs=4"
+    static let privacyPolicy = "https://www.notion.so/981178e9bcf5492ba7b75e6599ee936d?pvs=4"
+    static let consentMarketing = "https://www.notion.so/34ea1a31615543c097f46ecbd9a516ea?pvs=4"
     
     static let meltGithub = "https://github.com/meltsplit"
     static let hidiGithub = "https://github.com/HELLOHIDI"

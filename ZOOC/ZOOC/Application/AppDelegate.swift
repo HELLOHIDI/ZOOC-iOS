db3bdfa6985dc5b4454ff88cc61ecd9ad2a0c1f4
Melt <seokwoo2000@naver.com>
2023-08-03 12:52:13 +0900

    [SET] #187 - Git ignore 세팅 및 전처리 디버그 / 릴리즈 확인

--- ZOOC/ZOOC/Application/AppDelegate.swift
+++ ZOOC/ZOOC/Application/AppDelegate.swift
@@ -22,11 +22,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         print("👼🏻 \(#function)")
-        
+        print(Config.baseURL)
+        #if DEBUG
+        print("디버그야!!!")
+        #else
+        print("디버그 아니야!!!")
+        #endif
         setUserNotification(application)
         setKaKaoSDK()
         setFirebaseMessaging()
         setSentry()
+        
+        
         
         return true
     }
---
+++ ZOOC/ZOOC/Configuration/Config Docs.md
@@ -0,0 +1,25 @@
+#  Configuration 정보
+
+## Configuration은 총 4개의 파일이 존재한다.
+- Config Docs
+- GoogleService-Info.plist
+- Debug.xcconfig
+- Release.xcconfig
+
+### Config Docs
+- 현재 보고 있는 파일
+
+### GoogleService-Info.plist
+- Firebase를 사용하기 위해 필요한 파일
+
+### Debug.xcconfig, Release.xcconfig
+- Debug / Release 빌드를 나누기 위해 존재하는 파일
+
+### 각 파일이 담고있는 Key-Value
+
+- App Name
+- Base URL
+- Kakao SDK App Key
+- 
+
+
